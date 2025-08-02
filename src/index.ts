0c99ef717751b4eeb89159bd53507997696dc071
frectonz <fraol0912@gmail.com>
2025-08-02 22:15:20 +0300

    feat: add server plugin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,87 @@
-export const one = "one";
+import { type User } from "better-auth";
+import { betterFetch } from "@better-fetch/fetch";
+import { genericOAuth } from "better-auth/plugins";
+import { decodeJwt, importJWK, SignJWT } from "jose";
+
+const DISCOVERY_URL =
+  "https://esignet.ida.fayda.et/.well-known/openid-configuration";
+const USER_INFO_URL = "https://esignet.ida.fayda.et/v1/esignet/oidc/userinfo";
+const TOKEN_ENDPOINT = "https://esignet.ida.fayda.et/v1/esignet/oauth/v2/token";
+
+export interface FaydaOptions {
+  clientId: string;
+  privateKey: string;
+  redirectUrl: string;
+}
+
+export const fayda = async ({
+  clientId,
+  privateKey,
+  redirectUrl,
+}: FaydaOptions) => {
+  genericOAuth({
+    config: [
+      {
+        providerId: "fayda",
+        clientId,
+        clientSecret: "",
+
+        discoveryUrl: DISCOVERY_URL,
+        redirectURI: redirectUrl,
+
+        tokenUrlParams: {
+          client_assertion: await generateSignedJwt(clientId, privateKey),
+          client_assertion_type:
+            "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+        },
+
+        async getUserInfo(tokens) {
+          const userInfo = await betterFetch<Blob>(USER_INFO_URL, {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${tokens.accessToken}`,
+            },
+            retry: 5,
+          });
+
+          const jwt = await userInfo.data?.text()!;
+          const user = decodeJwt(jwt);
+
+          return {
+            id: user?.sub,
+            emailVerified: user?.email_verified,
+            email: user?.email,
+            image: user?.picture,
+            name: user?.name,
+            ...userInfo.data,
+          } as User;
+        },
+      },
+    ],
+  });
+};
+
+async function generateSignedJwt(clientId: string, privateKey: string) {
+  const header = {
+    alg: "RS256",
+    typ: "JWT",
+  };
+
+  const payload = {
+    iss: clientId,
+    sub: clientId,
+    aud: TOKEN_ENDPOINT,
+  };
+
+  const decodedKey = Buffer.from(privateKey, "base64").toString();
+  const jwkObject = JSON.parse(decodedKey);
+  const privateKeyJwk = await importJWK(jwkObject, "RS256");
+
+  const jwt = await new SignJWT(payload)
+    .setProtectedHeader(header)
+    .setIssuedAt()
+    .setExpirationTime("2h")
+    .sign(privateKeyJwk);
+
+  return jwt;
+}
